d8834ab9a3a04de4ea6a477bef932bffa8c47212
jevon wirjono <kurisaq@gmail.com>
2021-08-17 18:26:46 +0700

    Development phase 1

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(db,{
+            //got message depreciated
+            useNewUrlParser: true,
+            useUnifiedTopology : true
+        });
+        console.log('MongoDB Connected');
+    }
+    catch(err){
+        console.error(err.message);
+        //exit process
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
+
+//Mongoose connect mongodb to house apps
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`))
+
+
+
+//Used for starting web server
+//npm run server
