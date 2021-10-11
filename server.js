e12d6d49d93071976bcd78d9575d0e3fbedec57c
jevon wirjono <kurisaq@gmail.com>
2021-10-11 15:38:20 +0700

    preparation for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { connect } = require('mongoose');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,7 +10,8 @@ app.use(express.json({ extended: false }));
 //Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Dev Only
+//app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,6 +19,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/test', require('./routes/api/test'));
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //Set Static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 
 
